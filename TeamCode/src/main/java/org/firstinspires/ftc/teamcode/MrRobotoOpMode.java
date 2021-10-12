1b9731f92521943f0616cd4003947a5df3c2150a
Xmexy <azaman@browning.edu>
2021-10-11 23:26:35 -0400

    Adding OpMode Skeleton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MrRobotoOpMode.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="Mr Roboto Op Mode", group="TeleOp")
+public class MrRobotoOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
